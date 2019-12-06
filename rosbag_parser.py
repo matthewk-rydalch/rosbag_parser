0ba750672a9f65d6612a9791aa55e6bb5877f379
Taylor Pool <taylorpool.27@gmail.com>
2019-12-06 13:16:54 -0700

    Added relposheading in the graphs.

--- rosbag_parser.py
+++ rosbag_parser.py
@@ -12,19 +12,20 @@ def main():
 
 
 	#Multirover Test
-	testtitle = 'One Base Two Rovers'
+	testtitle = 'Multiple Rovers'
 	filenames = ['alldata.bag']
+	path = '/home/magicc/rtk_tests/tworoveronecomp/'
 	bagtitles = ['Data']
-	names = ['rover', 'rover2']
+	names = ['base', 'rover', 'rover2']
 
 	multiroverbags = []
 
 	for filename in filenames:
-		multiroverbags.append(rosbag.Bag('~/rtk_tests/tworoveronecomp/' + filename))
+		multiroverbags.append(rosbag.Bag(path + filename))
 
 	#Execute function to plot data from rosbags
 
-	multirotor_stdev = parserplot(testtitle, multiroverbags, bagtitles, names)
+	multirotor_stdev = parserplot(testtitle, multiroverbags, bagtitles, names, path)
 
 	# testtitle = 'Stationary Base and Rover'
 	# filenames = ['rover_northsouth.bag', 'rover_eastwest.bag', 'rover_diagonal.bag']
@@ -38,13 +39,11 @@ def main():
 
 	#stationary_stdev = parserplot(testtitle, stationary_base_roverbags, bagtitles)
 
-	plt.show()
-
 	print(multirotor_stdev)
 	#print(stationary_stdev)
 	#plt.close('all')
 
-def parserplot(testtitle, rosbags, bagtitles, names):
+def parserplot(testtitle, rosbags, bagtitles, names, path):
 
 	data = Parser();
 
@@ -63,21 +62,16 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 	#Create a for loop to go through all bags
 	for name in names:
 
-		#Get title of bag
-		bagtitle = bagtitles[index]
-
-		#Get name of which rover to parse data for
-		name = names[index];
-
 		#Get variables from the bag
 		variables = data.get_variables(bag, name = name)
-		set_trace();
+		#set_trace();
 
 		#Create blank arrays for Rel Position NED Values
 		north = []
 		east = []
 		up = []
 		time = []
+		relposheading = []
 
 		#print(variables.relPosNED)
 
@@ -91,6 +85,7 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 			north.append(variables.relPosNED[i][0])
 			east.append(variables.relPosNED[i][1])
 			up.append(-1.0*variables.relPosNED[i][2])
+			relposheading.append(variables.relPosHeading[i])
 			timestamp = (variables.secs_rel[i]-variables.secs_rel[0] +
 				 (variables.nsecs_rel[i]-variables.nsecs_rel[0])*10.0**(-9))
 			if timestamp < 0:
@@ -126,17 +121,19 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 				velup.append(-1.0*variables.velNED[i][2])
 
 		#Flags
-		figureflags = plt.figure()
-		figureflags.suptitle(bagtitle + ' Flags')
+		"""figureflags = plt.figure()
+		figureflags.suptitle(name + ' Flags')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('Flag')
 		#plt.axis([0,time[len(flags)-1], 0, stat.mean(flags) + stat.stdev(flags)])
 		plt.scatter(time, flags)
+		plt.savefig(path+name+'flags.png')
+		#set_trace()
 		#plt.legend()
 
 		#North Position
 		figurenorth = plt.figure()
-		figurenorth.suptitle(bagtitle + ' North Position')
+		figurenorth.suptitle(name + ' North Position')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('North (m)')
 		plt.axis([0, time[len(time)-1], stat.mean(north)-2*stat.stdev(north),
@@ -146,10 +143,11 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(time, [stat.mean(north)-stat.stdev(north)]*len(time), 'g--', label = 'Standard Deviation: %.3f meters' %stat.stdev(north))
 		plt.plot(time, [stat.mean(north)+stat.stdev(north)]*len(time), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'northpos.png')
 
 		#East Position
 		figureeast = plt.figure()
-		figureeast.suptitle(bagtitle + ' East Position')
+		figureeast.suptitle(name + ' East Position')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('East (m)')
 		plt.axis([0, time[len(time)-1], stat.mean(east)-2*stat.stdev(east),
@@ -159,10 +157,11 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(time, [stat.mean(east)-stat.stdev(east)]*len(time), 'g--', label = 'Standard Deviation: %.3f meters' %stat.stdev(east))
 		plt.plot(time, [stat.mean(east)+stat.stdev(east)]*len(time), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'eastpos.png')
 
 		#Up Position
 		figureup = plt.figure()
-		figureup.suptitle(bagtitle + ' Vertical Position')
+		figureup.suptitle(name + ' Vertical Position')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('Up')
 		plt.axis([0, time[len(time)-1], stat.mean(up)-2*stat.stdev(up),
@@ -172,10 +171,11 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(time, [stat.mean(up)-stat.stdev(up)]*len(time), 'g--', label = 'Standard Deviation: %.3f meters' %stat.stdev(up))
 		plt.plot(time, [stat.mean(up)+stat.stdev(up)]*len(time), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'vertpos.png')
 
 		#North Velocity
 		figurevelnorth = plt.figure()
-		figurevelnorth.suptitle(bagtitle + ' North Velocity')
+		figurevelnorth.suptitle(name + ' North Velocity')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('North Velocity (m/s)')
 		plt.axis([0, veltime[len(veltime)-1], stat.mean(velnorth)-2*stat.stdev(velnorth),
@@ -185,10 +185,11 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(veltime, [stat.mean(velnorth)-stat.stdev(velnorth)]*len(veltime), 'g--', label = 'Standard Deviation: %.3f m/s' %stat.stdev(velnorth))
 		plt.plot(veltime, [stat.mean(velnorth)+stat.stdev(velnorth)]*len(veltime), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'northvel.png')
 
 		#East Velocity
 		figureveleast = plt.figure()
-		figureveleast.suptitle(bagtitle + ' East Velocity')
+		figureveleast.suptitle(name + ' East Velocity')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('East Velocity (m/s)')
 		plt.axis([0, veltime[len(veltime)-1], stat.mean(veleast)-2*stat.stdev(veleast),
@@ -198,10 +199,11 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(veltime, [stat.mean(veleast)-stat.stdev(veleast)]*len(veltime), 'g--', label = 'Standard Deviation: %.3f m/s' %stat.stdev(veleast))
 		plt.plot(veltime, [stat.mean(veleast)+stat.stdev(veleast)]*len(veltime), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'eastvel.png')
 
 		#Up Velocity
 		figurevelup = plt.figure()
-		figurevelup.suptitle(bagtitle + ' Vertical Velocity')
+		figurevelup.suptitle(name + ' Vertical Velocity')
 		plt.xlabel('Time (sec)')
 		plt.ylabel('Vertical Velocity (m/s)')
 		plt.axis([0, veltime[len(veltime)-1], stat.mean(velup)-2*stat.stdev(velup),
@@ -211,22 +213,46 @@ def parserplot(testtitle, rosbags, bagtitles, names):
 		plt.plot(veltime, [stat.mean(velup)-stat.stdev(velup)]*len(veltime), 'g--', label = 'Standard Deviation: %.3f m/s' %stat.stdev(velup))
 		plt.plot(veltime, [stat.mean(velup)+stat.stdev(velup)]*len(veltime), 'g--')
 		plt.legend()
+		plt.savefig(path+name+'vertvel.png')"""
+
+		figurerelposheading = plt.figure()
+		figurerelposheading.suptitle(name + ' Relative Position Heading')
+		plt.xlabel('Time (sec)')
+		plt.ylabel('Heading (Radians)')
+		plt.axis([0, time[len(time)-1], stat.mean(relposheading)-2*stat.stdev(relposheading),
+			stat.mean(relposheading)+2*stat.stdev(relposheading)])
+		plt.scatter(time, relposheading, 1, label = 'Data')
+		plt.plot(time, [stat.mean(relposheading)]*len(time), 'r--', label = 'Mean Average: %.3f rad' %stat.mean(relposheading))
+		plt.plot(time, [stat.mean(relposheading)-stat.stdev(relposheading)]*len(time), 'g--', label = 'Standard Deviation: %.3f rad' %stat.stdev(relposheading))
+		plt.plot(time, [stat.mean(relposheading)+stat.stdev(relposheading)]*len(time), 'g--')
+		plt.legend()
+		plt.savefig(path+name+'relposheading.png')
 
 		#print(index)
 		index = index + 1
 
-		stddevnorth = stddevnorth + stat.stdev(north)
+		"""stddevnorth = stddevnorth + stat.stdev(north)
 		stddeveast = stddeveast + stat.stdev(east)
 		stddevup = stddevup + stat.stdev(up)
 		stddevvelnorth = stddevvelnorth + stat.stdev(velnorth)
 		stddevveleast = stddevveleast + stat.stdev(veleast)
-		stddevvelup = stddevvelup + stat.stdev(velup)
+		stddevvelup = stddevvelup + stat.stdev(velup)"""
+		plt.show();
+		#set_trace()
+		plt.close('all')
 
-		figures.extend([figureflags, figurenorth, figureeast, figureup, figurevelnorth,
-	          figureveleast, figurevelup])
+		#figures.extend([figureflags, figurenorth, figureeast, figureup, figurevelnorth,
+	          #figureveleast, figurevelup])
 
 	#Cycle through all the rosbags to get all the data.
 	#for rosbag in rosbags:
+	stddevnorth = 0
+	stddeveast = 0
+	stddevup = 0
+	stddevvelnorth = 0
+	stddevveleast = 0
+	stddevvelup = 0
+
 
 	stddevnorth = stddevnorth/index
 	stddeveast = stddeveast/index
@@ -245,6 +271,7 @@ class Parser:
 		relPosNEDHP = []
 		relPosLength = []
 		relPosHPLength = []
+		relPosHeading=[]
 		flags = []
 		secs_rel = []
 		nsecs_rel = []
@@ -273,6 +300,7 @@ class Parser:
 			flags.append(msg.flags)
 			secs_rel.append(msg.header.stamp.secs)
 			nsecs_rel.append(msg.header.stamp.nsecs)
+			relPosHeading.append(msg.relPosHeading)
 
 		for topic, msg, t in bag.read_messages(topics=['/'+name+'/PosVelTime']):
 			secs_pos.append(msg.header.stamp.secs)
@@ -304,11 +332,11 @@ class Parser:
 		# 	position.append(msg.position);
 
 		MyStruct = namedtuple("mystruct", "relPosNED, relPosNEDHP, relPosLength, relPosHPLength, \
-		flags, secs_rel, nsecs_rel, secs_pos, nsecs_pos, lla, year, month, day, hour, \
+		relPosHeading, flags, secs_rel, nsecs_rel, secs_pos, nsecs_pos, lla, year, month, day, hour, \
 		minute, sec, nano, position, meanXYZ, velNED")
 
 		variables = MyStruct(relPosNED, relPosNEDHP, relPosLength, relPosHPLength, \
-		flags, secs_rel, nsecs_rel, secs_pos, nsecs_pos, lla, year, month, day, hour, \
+		relPosHeading, flags, secs_rel, nsecs_rel, secs_pos, nsecs_pos, lla, year, month, day, hour, \
 		minute, sec, nano, position, meanXYZ, velNED)
 
 		return variables
