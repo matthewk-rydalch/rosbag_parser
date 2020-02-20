d9aa6a4aca2f94cfd0197a6377e1d59926eed5c5
Matt Rydalch <matthewk.rydalch@gmail.com>
2020-02-20 09:39:23 -0700

    debugging

--- rtk_flight/rosbag_parser.py
+++ rtk_flight/rosbag_parser.py
@@ -63,7 +63,7 @@ class Parser:
 			plt_z.append(msg.pose.position.z)
 			plt_pos = [plt_x, plt_y, plt_z]
 			plt_orientation.append(msg.pose.orientation)
-			plt = structure(plt_sec, plt_nsec, plt_pos, plt_orientation)
+			plt = pos_orient_time(plt_sec, plt_nsec, plt_x, plt_y, plt_z, plt_orientation)
 
 		for topic, msg, t in bag.read_messages(topics=['/platform_virtual_odometry']): #this is the estimate
 			plt_virt_sec.append(msg.header.stamp.secs)
@@ -73,7 +73,7 @@ class Parser:
 			plt_virt_z.append(msg.pose.pose.position.z)
 			plt_virt_pos = [plt_virt_x, plt_virt_y, plt_virt_z]
 			plt_virt_orientation.append(msg.pose.pose.orientation)
-			plt_virt = structure(plt_virt_sec, plt_virt_nsec, plt_virt_pos, plt_virt_orientation)
+			plt_virt = pos_orient_time(plt_virt_sec, plt_virt_nsec, plt_x, plt_y, plt_z, plt_virt_orientation)
 
 
 		for topic, msg, t in bag.read_messages(topics=['/ragnarok_ned']): #this is the estimate
@@ -84,7 +84,7 @@ class Parser:
 			drone_z.append(msg.pose.position.z)
 			drone_pos = [drone_x, drone_y, drone_z]
 			drone_orientation.append(msg.pose.orientation)
-			drone = structure(drone_sec, drone_nsec, drone_pos, drone_orientation)
+			drone = pos_orient_time(drone_sec, drone_nsec, plt_x, plt_y, plt_z, drone_orientation)
 
 		for topic, msg, t in bag.read_messages(topics=['/high_level_command']): #this is the estimate
 			hl_comd_sec.append(msg.header.stamp.secs)
@@ -94,7 +94,7 @@ class Parser:
 			hl_comd_z.append(msg.z)
 			hl_comd_pos = [hl_comd_x, hl_comd_y, hl_comd_z]
 			hl_comd_orientation.append(msg.F)
-			hl_comd = structure(hl_comd_sec, hl_comd_nsec, hl_comd_pos, hl_comd_orientation)
+			hl_comd = pos_orient_time(hl_comd_sec, hl_comd_nsec, plt_x, plt_y, plt_z, hl_comd_orientation)
 
 		for topic, msg, t in bag.read_messages(topics=['/is_flying']): #this is the estimate
 			is_flying.append(msg.data)
@@ -110,7 +110,7 @@ class Parser:
 			odom_z.append(msg.pose.pose.position.z)
 			odom_pos = [odom_x, odom_y, odom_z]
 			odom_orientation.append(msg.pose.pose.orientation)
-			odom = structure(odom_sec, odom_nsec, odom_pos, odom_orientation)
+			odom = pos_orient_time(odom_sec, odom_nsec, plt_x, plt_y, plt_z, odom_orientation)
 
 		MyStruct = namedtuple("mystruct", "plt, plt_virt, drone, hl_comd, is_flying, is_landing, odom")
 
@@ -118,11 +118,10 @@ class Parser:
 
 		return variables
 
-class structure:
-	def __init__(self, sec, nsec, position, orientation):
-		self.sec = sec
-		self.nsec = nsec
-		self.position = position
+class pos_orient_time:
+	def __init__(self, sec, nsec, x, y, z, orientation):
+		self.time = sec+nsec*1E-9
+		self.position = [x, y, z]
 		self.orientation = orientation
 
 if __name__ == '__main__':
