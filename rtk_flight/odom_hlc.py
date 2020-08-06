c4c64e0f0c86d086c62fffab81abe62e5efe892e
matt <matthewk.rydalch@gmail.com>
2020-08-06 15:40:50 -0600

    small changes

--- rtk_flight/odom_hlc.py
+++ rtk_flight/odom_hlc.py
@@ -58,8 +58,10 @@ def get_north_data(odom, hlc):
 	hlc_n_points = np.array(hlc.position[0])
 	hlc_time, hlc_n = level_hlc_data(hlc_time_points, hlc_n_points)
 
-	fig_num = 1
-	plot_2(fig_num, odom_time, odom_n, 'odom', hlc_time, hlc_n, 'hlc')
+	error_time, error_n = calc_error(odom_time, odom_n, hlc_time, hlc_n)
+
+	fig_title = 'north odometry, waypoints, and error'
+	plot_3_subplots(fig_title, odom_time, odom_n, 'odom', hlc_time, hlc_n, 'wps', error_time, error_n, 'error')
 
 
 def get_east_data(odom, hlc):
@@ -71,8 +73,10 @@ def get_east_data(odom, hlc):
 	hlc_e_points = np.array(hlc.position[1])
 	hlc_time, hlc_e = level_hlc_data(hlc_time_points, hlc_e_points)
 
-	fig_num = 2
-	plot_2(fig_num, odom_time, odom_e, 'odom', hlc_time, hlc_e, 'hlc')
+	error_time, error_e = calc_error(odom_time, odom_e, hlc_time, hlc_e)
+
+	fig_title = 'east odometry, waypoints, and error'
+	plot_3_subplots(fig_title, odom_time, odom_e, 'odom', hlc_time, hlc_e, 'wps', error_time, error_e, 'error')
 
 
 def get_height_data(odom, hlc):
@@ -84,8 +88,10 @@ def get_height_data(odom, hlc):
 	hlc_h_points = np.array(hlc.position[2])
 	hlc_time, hlc_h = level_hlc_data(hlc_time_points, hlc_h_points)
 
-	fig_num = 3
-	plot_2(fig_num, odom_time, odom_h, 'odom', hlc_time, hlc_h, 'hlc')
+	error_time, error_h = calc_error(odom_time, odom_h, hlc_time, hlc_h)
+
+	fig_title = 'height odometry, waypoints, and error'
+	plot_3_subplots(fig_title, odom_time, odom_h, 'odom', hlc_time, hlc_h, 'wps', error_time, error_h, 'error')
 
 
 def level_hlc_data(time, pos):
@@ -107,12 +113,51 @@ def level_hlc_data(time, pos):
 	return new_time, new_pos
 
 
+def calc_error(odom_time, odom, hlc_time, hlc):
+
+	error = []
+
+	i = 0
+	j = 0
+
+	#loops through points before the last waypoint
+	for j in range(len(hlc)):
+		while (odom_time[i] < hlc_time[j]):
+			error.append(hlc[j] - odom[i])
+			i = i+1
+
+	#this is for the last values after the last waypoint
+	for k in range(len(odom)-i):
+		error.append(error[i-1])
+	
+	error_time = odom_time
+
+	return error_time, error
+
+
 def plot_2(fig_num, t_x, x, xlabel, t_y, y, ylabel):
 
 	plt.figure(fig_num)
 	plt.plot(t_x, x, label = xlabel)
 	plt.plot(t_y, y, label = ylabel)
 	plt.legend(loc = "upper right")
+
+
+def plot_3_subplots(title, t_x, x, xlabel, t_y, y, ylabel, t_z, z, zlabel):
+
+	fig, sub = plt.subplots(2)
+	fig.suptitle(title)
+
+	sub[0].plot(t_x, x, label = xlabel)
+	sub[0].plot(t_y, y, label = ylabel)
+	sub[0].legend(loc = "upper right")
+	sub[0].set_ylabel('position')
+
+	#compare y
+	sub[1].plot(t_z, z, label = zlabel)
+	sub[1].legend(loc = "upper right")
+	sub[1].set_xlabel('t [s]')
+	sub[1].set_ylabel('error')
 
 
 if __name__ == '__main__':
