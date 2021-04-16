cfe843012de264b28e8128e0937edb32bb48f40e
matt <matthewk.rydalch@gmail.com>
2021-04-16 16:31:27 -0600

    Finished first try at px4 flight data collection.

--- px4_flight/get_flight_data.py
+++ px4_flight/get_flight_data.py
@@ -3,8 +3,7 @@ from IPython.core.debugger import set_trace
 import matplotlib.pyplot as plt
 import rosbag
 import numpy as np
-import navpy
-
+import statistics as stats
 def main():
 	flightModeTopic = '/flight_mode'
 	missionStateTopic = '/mission_state'
@@ -19,6 +18,25 @@ def main():
 
 	timeOfFlight,timeInterval = calc_time_of_flight(flightMode,missionState)
 	speedTimeIndeces = find_time_interval_indeces(gps,timeInterval)
+	speedMean,speedStdDev = calc_speed_data(gps,speedTimeIndeces)
+	accelTimeIndeces = find_time_interval_indeces(imu,timeInterval)
+	accelMean,accelStdDev = calc_accel_data(imu,accelTimeIndeces)
+	angRatesTimeIndeces = find_time_interval_indeces(imu,timeInterval)
+	angRatesMean,angRatesStdDev = calc_ang_rates_data(imu,angRatesTimeIndeces)
+
+	print('time of flight = ', timeOfFlight)
+
+	print('speed data:')
+	print('		mean = ', speedMean)
+	print('		std dev = ', speedStdDev)
+
+	print('accel data:')
+	print('		mean = ', accelMean)
+	print('		std dev = ', accelStdDev)
+
+	print('angular rates data:')
+	print('		mean = ', angRatesMean)
+	print('		std dev = ', angRatesStdDev)
 
 def calc_time_of_flight(flightMode,missionState):
 	for mode in flightMode:
@@ -51,8 +69,32 @@ def find_time_interval_indeces(x,timeInterval):
 	timeIntervalIndeces = [startIndex,endIndex]
 	return timeIntervalIndeces
 
+def calc_speed_data(gps,timeIndeces):
+	speedList = []
+	for i in range(timeIndeces):
+		normVelocityI = np.linalg.norm(gps.velocity)
+		speedList.append(normVelocityI)
+	speedMean = stats.mean(speedList)
+	speedStdDev = stats.stdev(speedList)
+	return speedMean,speedStdDev
 
-	 
+def calc_accel_data(imu,timeIndeces):
+	accelList = []
+	for i in range(timeIndeces):
+		normAccelI = np.linalg.norm(imu.accel)
+		accelList.append(normAccelI)
+	accelMean = stats.mean(accelList)
+	accelStdDev = stats.stdev(accelList)
+	return accelMean,accelStdDev
+
+def calc_ang_rates_data(imu,timeIndeces):
+	omegaList = []
+	for i in range(timeIndeces):
+		normOmegaI = np.linalg.norm(imu.omega)
+		omegaList.append(normOmegaI)
+	omegaMean = stats.mean(omegaList)
+	omegaStdDev = stats.stdev(omegaList)
+	return omegaMean,omegaStdDev
 
 def get_data(data, bag):
 	flightMode = data.get_flight_mode(bag)
